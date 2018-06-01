d09eecbc342cdeaa55967842b9b504e0c95fd705
Ariel Zhen <zhenpingping@live.com>
2018-06-01 19:23:21 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  @@all = [] 
+
+  def initialize(name) 
+    @name = name 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all.each do |dogs|
+      puts dogs
+    end   
+  end 
+
+  def self.clear_all 
+    @@all.clear 
+  end 
+end 
